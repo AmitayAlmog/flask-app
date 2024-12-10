c5e0c6c35bae44f2fb61f714c0438a9b57fea8f3
Amitay <amitay.almog@gmail.com>
2024-12-10 10:57:29 +0200

    second commit

--- app.py
+++ app.py
@@ -1,28 +1,62 @@
 from flask import Flask, render_template
 import os
 import random
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST", "localhost")  
+db_user = os.getenv("DB_USER", "root")       
+db_password = os.getenv("DB_PASSWORD", "")
+db_name = os.getenv("DB_NAME", "gifdatabase")
 
 app = Flask(__name__)
 
-# list of cat images
-images = [
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F0.gif?alt=media&token=0fff4b31-b3d8-44fb-be39-723f040e57fb",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F1.gif?alt=media&token=2328c855-572f-4a10-af8c-23a6e1db574c",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F10.gif?alt=media&token=647fd422-c8d1-4879-af3e-fea695da79b2",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F11.gif?alt=media&token=900cce1f-55c0-4e02-80c6-ee587d1e9b6e",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F2.gif?alt=media&token=8a108bd4-8dfc-4dbc-9b8c-0db0e626f65b",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F3.gif?alt=media&token=4e270d85-0be3-4048-99bd-696ece8070ea",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F4.gif?alt=media&token=e7daf297-e615-4dfc-aa19-bee959204774",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F5.gif?alt=media&token=a8e472e6-94da-45f9-aab8-d51ec499e5ed",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F7.gif?alt=media&token=9e449089-9f94-4002-a92a-3e44c6bd18a9",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F8.gif?alt=media&token=80a48714-7aaa-45fa-a36b-a7653dc3292b",
-    "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F9.gif?alt=media&token=a57a1c71-a8af-4170-8fee-bfe11809f0b3",
-]
+mydb = mysql.connector.connect(
+    host="db",
+    user="user",
+    password="password"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS gifdatabase")
+mydb.commit()
+
+mydb = mysql.connector.connect(
+  host="db",
+  user="user",
+  password="password",
+  database="gifdatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    url VARCHAR(255) NOT NULL
+)
+""")
+
+mycursor.execute("SELECT COUNT(*) FROM images")
+if mycursor.fetchone()[0] == 0:
+    initial_gifs = [
+        ("https://i.pinimg.com/originals/29/2a/50/292a50994d357e1884373ee8bf362562.gif",),
+        ("https://i.pinimg.com/originals/2f/e4/67/2fe467f5a4350854f383c2185f1da571.gif",),
+    ]
+    mycursor.executemany("INSERT INTO images (url) VALUES (%s)", initial_gifs)
+    mydb.commit()
 
 
 @app.route("/")
 def index():
-    url = random.choice(images)
+   
+    mycursor.execute("SELECT url FROM images")
+    gifs = [row[0] for row in mycursor.fetchall()]
+
+    url = random.choice(gifs) if gifs else None
     return render_template("index.html", url=url)
 
 
